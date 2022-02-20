d620d60208961bc7dc70cec5e67a380e538f44b5
LatvianRaccoon <arturs.svikis@gmail.com>
2022-02-20 16:20:45 +0100

    casting

---
+++ Casting/main.py
@@ -0,0 +1,30 @@
+
+# Python Casting
+# Integers
+a = int(1)
+b = int(2.8)
+c = int("3")
+
+print(a)
+print(b)
+print(c)
+
+# Floats
+a = float(1)
+b = float(2.8)
+c = float("3")
+d = float("4.2")
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Strings
+a = str("s1")
+b = str(2)
+c = str(3.0)
+
+print(a)
+print(b)
+print(c)
