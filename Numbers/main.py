1283e3b239b97d0ed3442c45538b5f89961b6155
LatvianRaccoon <arturs.svikis@gmail.com>
2022-02-20 16:10:24 +0100

    numbers and types of numbers

---
+++ Numbers/main.py
@@ -0,0 +1,65 @@
+
+# Python Numbers
+a = 1    # int
+b = 2.8  # float
+c = 1j   # complex
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Int
+a = 1
+b = 23456754356
+c = -65434567
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Float
+a = 1.10
+b = 1.0
+c = -35.26
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Float can also be scientific numbers with an "e" to indicate the power of 10
+a = 35e3
+b = 12E4
+c = -37.7e100
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Complex
+a = 3+5j
+b = 5j
+c = -5j
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Type Conversion
+x = 1
+y = 2.8
+z = 1j
+
+# convert from int to float
+a = float(x)
+# convert from float to int
+b = int(y)
+# convert from int to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
