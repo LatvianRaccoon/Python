c95fd2b542b10bb0b7d1345acd53609dbc7eb8a1
LatvianRaccoon <arturs.svikis@gmail.com>
2022-03-03 06:38:51 +0100

    web scraping

--- scraping/main.py
+++ scraping/main.py
@@ -10,8 +10,11 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 pokemon_table = soup.find('table', id = 'pokedex')
 row_data = []
-
-for row in pokemon_table.find_all('tr'):
+for title in pokemon_table.find_all('thead'):
+    col = title.find_all('th')
+    col = [element.text.strip() for element in col]
+    row_data.append(col)
+for row in pokemon_table.find_all('tr')[1:]:
     col = row.find_all('td')
     col = [element.text.strip() for element in col]
     row_data.append(col)
